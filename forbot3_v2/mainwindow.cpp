eef06c9dc07abcb37187001e9ec0833637815b76
JaKaczocha <jakub.kaczocha44295@gmail.com>
2023-12-07 19:13:15 +0100

    step 3 - android app

---
+++ forbot3_v2/mainwindow.cpp
@@ -0,0 +1,28 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    ui->textEdit_logs->insertPlainText(" 1 ");
+}
+
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    ui->textEdit_logs->insertPlainText(" 0 ");
+}
+
+
